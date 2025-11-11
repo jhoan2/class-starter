9c70f347fdc9ce9ff89c70a37940d73761602b61
John Hoang <johnhoang1994@gmail.com>
2025-11-10 16:37:55 -0800

    feat(privy): setting up privy wallets

---
+++ mobile/metro.config.js
@@ -0,0 +1,39 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Enable package exports for select libraries
+const resolveRequestWithPackageExports = (context, moduleName, platform) => {
+  // Package exports in `isows` (a `viem` dependency) are incompatible, so they need to be disabled
+  if (moduleName === 'isows') {
+    const ctx = {
+      ...context,
+      unstable_enablePackageExports: false,
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+
+  // Package exports in `zustand@4` are incompatible, so they need to be disabled
+  if (moduleName.startsWith('zustand')) {
+    const ctx = {
+      ...context,
+      unstable_enablePackageExports: false,
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+
+  // Package exports in `jose` are incompatible, so the browser version is used
+  if (moduleName === 'jose') {
+    const ctx = {
+      ...context,
+      unstable_conditionNames: ['browser'],
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+config.resolver.resolveRequest = resolveRequestWithPackageExports;
+
+module.exports = config;
